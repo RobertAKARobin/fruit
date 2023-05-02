852cfc7acc1295b0ca7c2cc6f1f3a69f04d2304c
RobertAKARobin <contact@robertakarobin.com>
2023-05-02 01:51:28 -0500

    Adds to cart

--- assets/index.js
+++ assets/index.js
@@ -3,7 +3,6 @@ const productsById = products.reduce((productsById, product) => {
 	productsById[product.name] = product;
 	return productsById;
 }, {});
-console.log(productsById);
 
 const $cartCounter = document.querySelector(`[data-cart-counter]`);
 const $buttonAtcs = Array.from(document.querySelectorAll(`[data-product-add]`));
@@ -12,8 +11,37 @@ for (const $buttonAtc of $buttonAtcs) {
 	$buttonAtc.addEventListener(`click`, handleAtc);
 }
 
+const cart = doTry(() => JSON.parse(localStorage.getItem(`cart`))) || {};
+setCartCount();
+
+function doTry(callback, catcher) {
+	try {
+		return callback();
+	} catch {
+		return catcher;
+	}
+}
+
 function handleAtc(event) {
 	const $button = event.currentTarget;
+	if ($button.getAttribute(`disabled`) === `disabled`) {
+		return;
+	}
+
 	const productName = $button.getAttribute(`data-product-add`);
-	console.log(productName);
+	$button.setAttribute(`disabled`, `disabled`);
+
+	setTimeout(() => {
+		$button.removeAttribute(`disabled`);
+		cart[productName] = (cart[productName] || 0) + 1;
+		localStorage.setItem(`cart`, JSON.stringify(cart));
+		setCartCount();
+	}, 800);
+}
+
+function setCartCount() {
+	const cartCount = products.reduce((cartCount, product) => {
+		return cartCount + (cart[product.name] || 0);
+	}, 0);
+	$cartCounter.textContent = cartCount === 0 ? '' : cartCount;
 }
