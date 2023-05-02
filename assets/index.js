f59ab5efa1837ad34f93e0f8f130f65699092555
RobertAKARobin <contact@robertakarobin.com>
2023-05-02 01:21:39 -0500

    Add json

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,19 @@
+const products = await (await fetch(`/assets/data/products.json`)).json();
+const productsById = products.reduce((productsById, product) => {
+	productsById[product.name] = product;
+	return productsById;
+}, {});
+console.log(productsById);
 
+const $cartCounter = document.querySelector(`[data-cart-counter]`);
+const $buttonAtcs = Array.from(document.querySelectorAll(`[data-product-add]`));
+
+for (const $buttonAtc of $buttonAtcs) {
+	$buttonAtc.addEventListener(`click`, handleAtc);
+}
+
+function handleAtc(event) {
+	const $button = event.currentTarget;
+	const productName = $button.getAttribute(`data-product-add`);
+	console.log(productName);
+}
